181600043ac548e4b0ed8f3bdac30418c66e4444
pntrgergo <pntrgergo@gmail.com>
2022-12-14 14:45:01 +0100

    Feat: Nav-bar

--- BitFit/Models/Food.cs
+++ BitFit/Models/Food.cs
@@ -2,5 +2,15 @@
 {
     public class Food
     {
+        public int Sugar { get; set; }
+        public int Fiber { get; set; }
+        public int ServingSize { get; set; }
+        public int Sodium { get; set; }
+        public string Name { get; set; }
+        public int Potassium { get; set; }
+        public int FatSaturated { get; set; }
+        public int FatTotal { get; set; }
+        public int Cholesterol { get; set; }
+        public int Carbohydrates { get; set; }
     }
 }
